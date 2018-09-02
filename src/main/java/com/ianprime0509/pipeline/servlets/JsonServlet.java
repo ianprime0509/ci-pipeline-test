f7434ca41eb717d69e87a78ec67d96af1f1a3ac1
Ian Johnson <ianprime0509@gmail.com>
2018-09-02 13:14:42 -0400

    Add another servlet for testing

---
+++ src/main/java/com/ianprime0509/pipeline/servlets/JsonServlet.java
@@ -0,0 +1,20 @@
+package com.ianprime0509.pipeline.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/json")
+public class JsonServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.getWriter().println("{\"key\": \"value\"}");
+    }
+}
